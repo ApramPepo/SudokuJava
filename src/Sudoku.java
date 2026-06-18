8205b00feed398e68b8938781b0491294608be94
ApramPepo <pepoapram2016@gmail.com>
2026-06-19 01:30:56 +0300

    Refactored the code and made it cleaner

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,6 +3,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Sudoku {
+    private final Font Font_Large = new Font("Calibri", Font.BOLD, 30);
+    private final Font Font_Medium = new Font("Calibri", Font.BOLD, 28);
+    private final Font Font_Small = new Font("Calibri", Font.BOLD, 24);
+
+    private final Color Color_BG_Locked = new Color(230, 230, 230);
+    private final Color Color_BG_Selected = new Color(180, 220, 250);
+    private final Color Color_BG_Correct = new Color(180, 250, 180);
+    private final Color Color_BG_Wrong = new Color(250, 200, 200);
+    private final Color Color_Blue_Text = new Color(50, 150, 250);
+
     private Tile selectedTile = null;
     private int mistakesCounter = 0;
 
@@ -18,75 +28,7 @@ public class Sudoku {
             {7, 9, 6, 3, 1, 8, 4, 5, 2}
     };
 
-    class Tile extends JButton {
-        private int row, col;
-        private int value;
-
-        public Tile(int row, int col, int value) {
-            this.row = row;
-            this.col = col;
-            this.value = value;
-
-            setFont(new Font("Calibri", Font.BOLD, 28));
-            setFocusPainted(false);
-            setBorder(BorderFactory.createLineBorder(Color.GRAY));
-
-            if (value != 0) {
-                setText(String.valueOf(value));
-                setForeground(Color.BLACK);
-                setBackground(new Color(230,230,230));
-                setEnabled(false);
-            } else {
-                setText("");
-                setForeground(new Color(50,150,250));
-                setBackground(Color.WHITE);
-            }
-
-            addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    handleTileClick();
-                }
-            });
-        }
-
-        private void handleTileClick() {
-            if(selectedTile != null && selectedTile.isEnabled()) {
-                selectedTile.setBackground(Color.WHITE);
-            }
-
-            if (this.isEnabled()) {
-                selectedTile = Tile.this;
-                selectedTile.setBackground(new Color(180,220,250));
-            } else {
-                selectedTile = null;
-            }
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-            setText(String.valueOf(value));
-
-            if (this.value == solvedBoard[row][col]) {
-                setForeground(new Color(50,150,250));
-                setBackground(new Color(180, 250, 180));
-                board[row][col] = value;
-            } else {
-                mistakesCounter++;
-                textLabel.setText("Sudoku: " + mistakesCounter + " Mistakes");
-                setForeground(Color.RED);
-                setBackground(new Color(250,200,200));
-                board[row][col] = value;
-            }
-        }
-
-        public int getRow() { return row; }
-        public int getCol() { return col; }
-        public int getTileValue() { return value; }
-    }
-
-
-    int[][] board = {
+    private final int[][] board = {
             { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
             { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
@@ -98,13 +40,13 @@ public class Sudoku {
             { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
     };
 
-    private final int width = 600;
-    private final int height = 800;
-    JFrame frame = new JFrame("Sudoku Game");
-    JLabel textLabel = new JLabel();
-    JPanel textPanel = new JPanel();
-    JPanel boardPanel = new JPanel();
-    JPanel controlPanel = new JPanel();
+    private int width = 600;
+    private int height = 800;
+    private final JFrame frame = new JFrame("Sudoku Game");
+    private final JLabel textLabel = new JLabel();
+    private final JPanel textPanel = new JPanel();
+    private final JPanel boardPanel = new JPanel();
+    private final JPanel controlPanel = new JPanel();
 
     Sudoku() {
         frame.setSize(width, height);
@@ -113,7 +55,7 @@ public class Sudoku {
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
 
-        textLabel.setFont(new Font("Calibri", Font.BOLD, 30));
+        textLabel.setFont(Font_Large);
         textLabel.setHorizontalAlignment(JLabel.CENTER);
         textLabel.setText("Sudoku: " + mistakesCounter + " Mistakes");
 
@@ -139,13 +81,17 @@ public class Sudoku {
                 }
             }
         });
-        frame.setFocusable(true);
 
-        /*
-          Keep @setVisible at the end
+         /*
+          Keep @setVisible and @setFocusable at the end
          */
+        frame.setFocusable(true);
         frame.setVisible(true);
     }
+
+    /*
+    Abstractions
+     */
 
     void setupTiles() {
         for (int r = 0; r < 9; r++) {
@@ -156,6 +102,76 @@ public class Sudoku {
         }
     }
 
+    class Tile extends JButton {
+        private final int row, col;
+        private int value;
+
+        public Tile(int row, int col, int value) {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+
+            setFont(Font_Medium);
+            setFocusPainted(false);
+            setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+            if (value != 0) {
+                setText(String.valueOf(value));
+                setForeground(Color.BLACK);
+                setBackground(Color_BG_Locked);
+                setEnabled(false);
+            } else {
+                setText("");
+                setForeground(new Color(50,150,250));
+                setBackground(Color.WHITE);
+            }
+
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    handleTileClick();
+                }
+            });
+        }
+
+
+        private void handleTileClick() {
+            if(selectedTile != null && selectedTile.isEnabled()) {
+                selectedTile.setBackground(Color.WHITE);
+            }
+
+            if (this.isEnabled()) {
+                selectedTile = Tile.this;
+                selectedTile.setBackground(Color_BG_Selected);
+            } else {
+                selectedTile = null;
+            }
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+            setText(String.valueOf(value));
+
+            if (this.value == solvedBoard[row][col]) {
+                setForeground(new Color(50,150,250));
+                setBackground(Color_BG_Correct);
+                board[row][col] = value;
+            } else {
+                mistakesCounter++;
+                textLabel.setText("Sudoku: " + mistakesCounter + " Mistakes");
+                setForeground(Color.RED);
+                setBackground(Color_BG_Wrong);
+                board[row][col] = value;
+            }
+        }
+
+        public int getRow() { return row; }
+        public int getCol() { return col; }
+        public int getTileValue() { return value; }
+    }
+
+
+
     void setControlPanel() {
         controlPanel.setLayout(new GridLayout(1,9,5,5));
         controlPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
@@ -163,7 +179,7 @@ public class Sudoku {
         for (int i = 0; i <= 9; i++) {
             final int num = i;
             JButton numButton = new JButton(String.valueOf(num));
-            numButton.setFont(new Font("Calibri", Font.BOLD, 24));
+            numButton.setFont(Font_Small);
             numButton.setFocusPainted(false);
 
             numButton.addActionListener(new ActionListener() {
